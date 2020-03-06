d4debadcd55373bc0b69eed9c4b962869f0ec05e
Suhas-G <suhas.g96.sg@gmail.com>
2020-03-07 00:10:34 +0530

    feat(api): Registration API Allow user to register by providing their details Check registration status.

--- app/api/api/urls.py
+++ app/api/api/urls.py
@@ -20,5 +20,6 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', views.index)
+    path('api/', views.index),
+    path('api/', include('authentication.urls'))
 ]
---
+++ app/api/authentication/tests/test_models.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+
+from authentication.models import Profile
+
+class ProfileModelTest(TestCase):
+
+    EMAIL = 'test@email.com'
+    NAME = 'test'
+    IS_JUNIOR_FELLOW = True
+    CAMPUS = 'University of the World'
+    BATCH = 2020
+    NUMBER = '99999999999'
+    COUNTRY_CODE = '+91'
+
+    @classmethod
+    def setUpTestData(cls):
+        Profile.objects.create(email = cls.EMAIL,
+                                name = cls.NAME,
+                                is_junior_fellow = cls.IS_JUNIOR_FELLOW,
+                                campus = cls.CAMPUS,
+                                batch = cls.BATCH,
+                                number = cls.NUMBER,
+                                country_code = cls.COUNTRY_CODE)
+    
+    def test_user_field(self):
+        profile = Profile.objects.get(pk = 1)
+        user = profile.user
+        self.assertEqual(user.email, self.EMAIL)
+        self.assertIsNone(user.username)
+        self.assertFalse(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_profile(self):
+        profile = Profile.objects.get(pk = 1)
+        self.assertEqual(profile.name, self.NAME)
+        self.assertEqual(profile.is_junior_fellow, self.IS_JUNIOR_FELLOW)
+        self.assertEqual(profile.campus, self.CAMPUS)
+        self.assertEqual(profile.batch, self.BATCH)
+
+    def test_phone_number_field(self):
+        profile = Profile.objects.get(pk = 1)
+        phone_number = profile.phone_number.first()
+        self.assertEqual(phone_number.number, self.NUMBER)
+        self.assertEqual(phone_number.country_code, self.COUNTRY_CODE)
