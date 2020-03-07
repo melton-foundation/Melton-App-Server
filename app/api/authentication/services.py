76939386e9f9c1f0c607066cd402f6aa0d7f72d1
Suhas-G <suhas.g96.sg@gmail.com>
2020-03-07 23:11:11 +0530

    feat(api): Registration API Send back token after registration Make all Json request and response follow camel casing

--- app/api/authentication/services.py
+++ app/api/authentication/services.py
@@ -1,15 +1,18 @@
 from rest_framework import status
 
 from authentication.serializers import ProfileSerializer, RegistrationStatusSerializer
+from authentication.models import AppUser, ExpiringToken
 
 
 def register_user(data):
     serializer = ProfileSerializer(data=data)
 
     if serializer.is_valid():
-        _ = serializer.save()
+        profile = serializer.save()
+        token = get_token(user=profile.user)
         response_status = status.HTTP_201_CREATED
-        response = {"type": "success", "message": "User created successfully"}
+        response = {"type": "success", "appToken": token.key if not token is None else None,
+                    "message": "User created successfully"}
     else:
         response, response_status = _form_bad_request_response(
             serializer.errors)
@@ -30,7 +33,7 @@ def check_registration(data):
             approved = "Approved" if is_active else "Pending"
             response = {"type": "success",
                         "email": serializer.validated_data["email"],
-                        "is_active": is_active,
+                        "isApproved": is_active,
                         "message": f"Your registration is {approved}"}
             response_status = status.HTTP_200_OK
     else:
@@ -38,6 +41,16 @@ def check_registration(data):
             serializer.errors)
 
     return response, response_status
+
+
+def get_token(email=None, user=None):
+    token = None
+    if user is not None:
+        token, _ = ExpiringToken.objects.get_or_create(user=user)
+    elif email is not None:
+        token, _ = ExpiringToken.objects.get_or_create_by_email(email=email)
+
+    return token
 
 
 def _form_bad_request_response(errors):
