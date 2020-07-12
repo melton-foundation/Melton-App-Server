944e52c32aa3d5db150dde4f7e79b0fb0dc3ec2d
Suhas-G <suhas.g96.sg@gmail.com>
2020-07-12 11:37:19 +0530

    feat (profile): allow creating super user

--- app/api/authentication/admin.py
+++ app/api/authentication/admin.py
@@ -14,12 +14,12 @@ class AppUserAdmin(UserAdmin):
     list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser')}
          ),
     )
     search_fields = ('email',)
