d9ab656f0aef3bdad124de73b2cb034df073d931
Arun Joseph <aarunjosephraj@gmail.com>
2020-07-24 20:49:22 +0530

    Update views.py

--- app/api/api/views.py
+++ app/api/api/views.py
@@ -2,29 +2,29 @@ from django.http import JsonResponse
 from sentry_sdk import capture_message
 from django.shortcuts import render
 
-def error_400_view(request, exception):
+def error_400_view(request):
     id = capture_message("Bad Request (400)", level="error")
     data = {"event_id": id,
             "code": 400,
             "title": "BAD REQUEST",
             "description": "The server cannot process the request due to a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing)."}
-    return render(request, data)
+    return JsonResponse(data)
 
-def error_403_view(request, exception):
+def error_403_view(request):
     id = capture_message("Forbidden (403)", level="error")
     data = {"event_id": id,
             "code": 403,
             "title": "FORBIDDEN",
             "description": "The client has insufficient authentication credentials for the server to process this request."}
-    return render(request, data)
+    return JsonResponse(data)
 
-def error_404_view(request, exception):
+def error_404_view(request):
     id = capture_message("Not Found (404)", level="error")
     data = {"event_id": id,
             "code": 404, "title":
             "NOT FOUND", "description":
             "The server is not able to find the requested resource. AKA, Page Not Found."}
-    return render(request, data)
+    return JsonResponse(data)
 
 def error_500_view(request):
     id = capture_message("Internal Server Error (500)", level="error")
@@ -32,7 +32,7 @@ def error_500_view(request):
             "code": 500,
             "title": "INTERNAL SERVER ERROR",
             "description": "The server encountered an unexpected condition that prevented it from fulfilling the request."}
-    return render(request, data)
+    return JsonResponse(data)
 
 def index(request):
     json = {
